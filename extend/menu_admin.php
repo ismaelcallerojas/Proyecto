ed33490400f98c7a38c7eeb49058457f02ed18ca
alex <alex.averanga@uab.edu.bo>
2021-10-26 14:52:59 -0400

    Plantilla del proyecto

---
+++ extend/menu_admin.php
@@ -0,0 +1,22 @@
+<nav class="black">
+    <span class="right"> Sistema Web: Inmobiliaria</span>
+    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+</nav>
+
+<ul id="slide-out" class="sidenav sidenav-fixed nav-wrapper">
+    <li>
+        <div class="user-view">
+            <div class="background">
+                <img src="../img/bg1.jpg" width="100%" height="100%">
+            </div> 
+            <a href="#"><img class="circle" src="img/bg2.jpg" ></a>
+            <a href="#" class="white-text"><b>Usuario</b></a>
+            <a class="white-text">correoejemplogmail.com</a>
+        </div>
+    </li>
+    <li><a href="../inicio"><i class="material-icons" >home</i>INICIO</a></li>
+
+    <li><div class="divider"></div></li>
+
+   
+</ul>
---
+++ extend/scripts.php
@@ -0,0 +1,7 @@
+</main>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="../js/v0.100.2/materialize.min.js"></script>
+<script src="../js/materialize.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.3.2/sweetalert2.js"></script>
