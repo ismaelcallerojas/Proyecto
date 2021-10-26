3d695ee8f3c11674869204d119e781932e505ed9
ismael <ismaelcallerojas@gmail.com>
2021-10-26 15:08:23 -0400

    Mostrar los registros en un tabla
    Ya que tenemos los registros, tambien implementamos un buscador
     de registros

--- usuarios/index.php
+++ usuarios/index.php
@@ -67,7 +67,77 @@ include '../extend/permiso.php';
     </div>
 </div>
 <!--Fin del buscador de usuarios -->
->
+
+<!--Mostrar datos de la tabla usuarios -->
+<?php 
+$sel = $con->query("SELECT * FROM usuario");
+$row = mysqli_num_rows($sel);
+?>
+<div class="row">
+    <div class="col s12">
+        <div class="card">
+            <div class="card-content">
+                <span class="card-title">Usuarios (<?php echo $row ?>)</span>
+                <table>
+                    <thead>
+                        <th>Nick</th>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Nivel</th>
+                        <th></th>
+                        <th>Foto</th>
+                        <th>Bloqueo</th>
+                        <th>Eliminar</th>
+                        
+                    </thead>
+                    <?php while($f = $sel->fetch_assoc()){ ?>
+                    <tr>
+                        <td><?php echo $f['nick'] ?></td>
+                        <td><?php echo $f['nombre'] ?></td>
+                        <td><?php echo $f['correo'] ?></td>
+                        <td>
+                        <form action="up_nivel.php" method="post">
+                            <input type="hidden" name="id" value="<?php echo $f['id'] ?>">
+                            <select name="nivel" required>
+                                <option value="<?php echo $f['nivel']?>" disabled selected><?php echo $f['nivel']?></option>
+                                <option value="ADMINISTRADOR">ADMINISTRADOR</option>
+                                <option value="ASESOR">ASESOR</option>
+                            </select>
+                        </td>
+                        <td>
+                            <button type="submit" class="btn-floating btn-small waves-effect waves-light"><i class="material-icons">repeat</i></button>
+                        </form>                    
+                        </td>
+                        <td><img src="<?php echo $f['foto'] ?>" width="50" class="circle"></td>
+                        <td style="text-align:center">
+                        <?php
+                        if ($f['bloqueo']==1): ?>
+                        <a href="bloqueo_manual.php?us=<?php echo $f['id']?>&bl=<?php echo $f['bloqueo']?>"><i class="material-icons green-text">lock_open</i></a>
+                        <?php else: ?>                        
+                            <a href="bloqueo_manual.php?us=<?php echo $f['id']?>&bl=<?php echo $f['bloqueo']?>"><i class="material-icons red-text">lock_outline</i></a>
+                                                
+                        <?php endif; ?>
+                        </td>
+                        <td style="text-align:center">
+                            <button onclick="swal({
+                                title: 'Esta seguro que desea eliminar al usuario?',
+                                text: 'Al eliminarlo, no podra recuperarlo!',
+                                type: 'warning',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: 'Si, eliminarlo!'
+                                }).then(function() {
+                                    location.href='eliminar_usuario.php?id=<?php echo $f['id'] ?>';})"" class="btn-floating btn-small waves-effect waves-light red" >
+                                    <i class="material-icons">clear</i></button>
+                        </td>
+                    </tr>
+                    <?php  } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</div><!--FIN Mostrar datos de la tabla usuarios -->
 
 <?php include '../extend/scripts.php'; ?>
     <script src="../js/validacion.js"></script>
