2197db72dd2f2b765e4bcaebdacda74c121f8fdf
Ismael Fernando Calle Rojas <92237023+ismaelcallerojas@users.noreply.github.com>
2021-10-26 20:56:19 -0400

    comentando la codificacion

--- admin/conexion/conexion.php
+++ admin/conexion/conexion.php
@@ -1,6 +1,6 @@
 <?php @session_start();
 $con = new mysqli('localhost','root','','inmobiliaria');
 
-//Codificando proyecto como utf8
+//Codificando los caracteres del proyecto como utf8
 $con->set_charset('utf8');
 ?>
