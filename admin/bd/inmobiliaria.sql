e3a135189809804dc78f3b4cefb605dbad754d2b
alvaro <blanco2723@gmail.com>
2021-10-28 11:37:21 -0400

    correccion envio de formulario

--- admin/bd/inmobiliaria.sql
+++ admin/bd/inmobiliaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2021 a las 16:26:44
+-- Tiempo de generación: 28-10-2021 a las 17:36:17
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.4.25
 
